37970d149eabdf8e581b62cb48b079f40d91b29b
AtaBerk <sucyro124@gmail.com>
2025-09-24 22:50:02 +0300

    muhtelif

---
+++ src/client/java/atatale/modid/UndertaleModDataGenerator.java
@@ -0,0 +1,11 @@
+package atatale.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UndertaleModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
